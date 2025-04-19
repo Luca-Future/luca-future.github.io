2b88392f921c13d4acffc1dca11830d94f1231a5
ZhangZhe <alexzhangaccn@gmail.com>
2025-04-19 23:07:05 +0800

    feat: edit homepage text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,15 +22,16 @@ export default function Home() {
     <>
       <ScrollUp />
       <Hero />
-      <Features />
-      <Video />
-      <Brands />
-      <AboutSectionOne />
-      <AboutSectionTwo />
-      <Testimonials />
-      <Pricing />
-      <Blog />
-      <Contact />
+      
     </>
   );
 }
+//<Features />
+//<Video />
+//<Brands />
+//<AboutSectionOne />
+//<AboutSectionTwo />
+//<Testimonials />
+//<Pricing />
+//<Blog />
+//<Contact />
